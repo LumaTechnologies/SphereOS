eca81c67657461b909a3befb7b537031e09df637
Jspa2 <73358272+Jspa2@users.noreply.github.com>
2023-01-06 18:57:34 +0000

    nextcore test

---
+++ NextCore/UI/Drawings/ClipDrawingElement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextCore.UI.Drawings
+{
+    public class ClipDrawingElement : DrawingElement
+    {
+    }
+}
---
+++ NextCore/UI/Drawings/RasterisableDrawingElement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextCore.UI.Drawings
+{
+    public abstract class RasterisableDrawingElement : DrawingElement
+    {
+        public abstract void Rasterise(int[] buffer, int offsetX, int offsetY);
+    }
+}
